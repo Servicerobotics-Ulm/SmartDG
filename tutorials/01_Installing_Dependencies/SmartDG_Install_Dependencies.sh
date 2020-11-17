14eed9afa5f51741952731aeebbc3f65ac264acd
Vineet <vineet.nagrath@gmail.com>
2020-11-17 13:06:34 +0100

    Added dependencies installation bash script

---
+++ tutorials/01_Installing_Dependencies/SmartDG_Install_Dependencies.sh
@@ -0,0 +1,45 @@
+YOPTION="-y"
+#YOPTION=""
+echo "########################################## SmartDG_Install_Dependencies START"
+echo "########################################## Updateing apt-get"
+sudo apt-get update
+echo "########################################## Installing build-essential"
+sudo apt-get $YOPTION install build-essential
+echo "########################################## Installing cmake"
+sudo apt $YOPTION install cmake
+echo "########################################## Installing git"
+sudo apt $YOPTION install git
+echo "########################################## Installing libx11-dev"
+sudo apt $YOPTION install libx11-dev
+echo "########################################## Installing libglu1-mesa-dev"
+sudo apt-get $YOPTION install libglu1-mesa-dev
+echo "########################################## Installing freeglut3-dev"
+sudo apt-get $YOPTION install freeglut3-dev
+echo "########################################## Installing mesa-common-dev"
+sudo apt-get $YOPTION install mesa-common-dev
+echo "########################################## Installing libxft-dev"
+sudo apt-get $YOPTION install libxft-dev
+echo "########################################## Cloning fltk"
+mkdir -p ~/SOFTWARE/
+cd ~/SOFTWARE/
+sudo rm -r ~/SOFTWARE/fltk
+git clone https://github.com/fltk/fltk.git
+echo "########################################## Installing fltk"
+cd ~/SOFTWARE/fltk
+mkdir ~/SOFTWARE/fltk/build
+cd ~/SOFTWARE/fltk/build
+cmake ..
+make
+sudo make install
+echo "########################################## Installing openjdk-11-jdk"
+sudo apt-get $YOPTION install openjdk-11-jdk
+java --version
+echo "########################################## Installing nodejs"
+sudo apt $YOPTION install nodejs
+node -v
+echo "########################################## Installing npm"
+sudo apt $YOPTION install npm
+npm -v
+echo "########################################## Installing Browsersync"
+sudo npm $YOPTION install -g browser-sync
+echo "########################################## SmartDG_Install_Dependencies DONE "
